5cf3da34132ec9c10050a6c11d3e76433d5e4311
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-04-02 17:02:29 +0300

    add user.js-MCR

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,19 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
-    | FULLSTACK TEAM | NODEJS / EXPRESS |
+    NODEJS EXPRESS | Stock API
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
+const user = require("../controllers/user");
 /* ------------------------------------------------------- */
+// URL: /users
+
+router.route("/").get(user.list).post(user.create);
+
+router
+  .route("/:id")
+  .get(user.read)
+  .put(user.update)
+  .patch(user.update)
+  .delete(user.delete);
+/* ------------------------------------------------------- */
+module.exports = router;
