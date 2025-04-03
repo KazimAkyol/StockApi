75c1b166c025dce7740579e8b7923e0e0221b1c0
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-04-03 13:27:08 +0300

    add authentication.js

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -6,5 +6,20 @@
 
 module.exports = async (req, res, next) => {
 
+    const auth = req.headers?.authorization || null
+    const tokenKey = auth ? auth.split(' ')
+
+    if (tokenKey) {
+        if (tokenKey[0] == ' Token') {
+            
+            const tokenData = await Token.findOne({token: tokenKey[1]}).populate('userId')
+            req.user = tokenData ? tokenData.userId : null
+        } else if (tokenKey[0] == Bearer) {
+            jwt.verify(tokenKey[1], process.env.ACCESS_KEY, (err, userData) => userData ? req.user=userData : req.user=null {
+                
+            })
+        }
+    }
+
     next()
 }
